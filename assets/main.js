08d856899f0f21d2b4524cf29e200df51632a107
Feras <ferassaleh1980@gmail.com>
2022-08-18 21:39:32 +0200

    adding the project files

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+const changeColor1 = () => {
+    body.style.backgroundColor = 'rgb(49, 6, 70) ';
+    hl.style.color = 'rgb(4, 88, 28)';
+};
+const changeColor2 = () => {
+    body.style.backgroundColor = ' rgb(246, 196, 16)';
+    hl.style.color = 'rgb(28, 109, 202) ';
+};
+const changeColor3 = () => {
+    body.style.backgroundColor = 'rgb(246, 136, 9) ';
+    hl.style.color = 'rgb(5, 5, 5)';
+};
+const changeColor4 = () => {
+    body.style.backgroundColor = 'rgb(9, 4, 93) ';
+    hl.style.color = 'rgb(130, 193, 242)';
+};
